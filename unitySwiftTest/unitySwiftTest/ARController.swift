c9aa4e496c27998b24f458acfb4dae21a39400b1
Devin Lim <devin@opennetworking.org>
2018-02-28 00:14:02 -0800

    Separate main screen from AR screen

---
+++ unitySwiftTest/unitySwiftTest/ARController.swift
@@ -0,0 +1,73 @@
+//
+//  ARController.swift
+//  unitySwiftTest
+//
+//  Created by Devin Lim on 2/27/18.
+//  Copyright © 2018 Jeremy Ronquillo. All rights reserved.
+//
+
+import UIKit
+
+class ARController: UIViewController {
+    @IBOutlet var rotateSwitch: UISwitch!
+    let appDelegate = UIApplication.shared.delegate as? AppDelegate
+    
+    @objc func handleUnityReady() {
+        showUnitySubView()
+    }
+    @IBAction func backAction(_ sender: UIButton) {
+        // TODO: find the way to close the unity.
+        appDelegate?.stopUnity()
+        self.dismiss(animated: true, completion: nil)
+    }
+    @objc func handleUnityToggleRotation(_ n: NSNotification) {
+        if let isOn = n.userInfo?["isOn"] as? NSNumber {
+            rotateSwitch.isOn = isOn.boolValue
+        }
+    }
+    
+    @IBAction func handleSwitchValueChanged(sender: UISwitch) {
+        UnityPostMessage("NATIVE_BRIDGE", "RotateCube", sender.isOn ? "start" : "stop")
+    }
+    
+    func showUnitySubView() {
+        if let unityView = UnityGetGLView() {
+            // insert subview at index 0 ensures unity view is behind current UI view
+            view?.insertSubview(unityView, at: 0)
+            
+            unityView.translatesAutoresizingMaskIntoConstraints = false
+            let views = ["view": unityView]
+            let w = NSLayoutConstraint.constraints(withVisualFormat: "|-0-[view]-0-|", options: [], metrics: nil, views: views)
+            let h = NSLayoutConstraint.constraints(withVisualFormat: "V:|-75-[view]-0-|", options: [], metrics: nil, views: views)
+            view.addConstraints(w + h)
+        }
+    }
+    func showUnity() {
+        appDelegate?.startUnity()
+        NotificationCenter.default.addObserver(self, selector: #selector(handleUnityReady), name: NSNotification.Name("UnityReady"), object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(handleUnityToggleRotation(_:)), name: NSNotification.Name("UnityToggleRotation"), object: nil)
+        handleUnityReady()
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        showUnity()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- unitySwiftTest/unitySwiftTest/ViewController.swift
+++ unitySwiftTest/unitySwiftTest/ViewController.swift
@@ -1,48 +1,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet var rotateSwitch: UISwitch!
-    @IBOutlet weak var butt: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-    }
-    
-    @objc func handleUnityReady() {
-        showUnitySubView()
-    }
-    
-    @objc func handleUnityToggleRotation(_ n: NSNotification) {
-        if let isOn = n.userInfo?["isOn"] as? NSNumber {
-            rotateSwitch.isOn = isOn.boolValue
-        }
-    }
-    
-    @IBAction func handleSwitchValueChanged(sender: UISwitch) {
-        UnityPostMessage("NATIVE_BRIDGE", "RotateCube", sender.isOn ? "start" : "stop")
-    }
-    
-    func showUnitySubView() {
-        if let unityView = UnityGetGLView() {
-            // insert subview at index 0 ensures unity view is behind current UI view
-            view?.insertSubview(unityView, at: 0)
-            
-            unityView.translatesAutoresizingMaskIntoConstraints = false
-            let views = ["view": unityView]
-            let w = NSLayoutConstraint.constraints(withVisualFormat: "|-0-[view]-0-|", options: [], metrics: nil, views: views)
-            let h = NSLayoutConstraint.constraints(withVisualFormat: "V:|-75-[view]-0-|", options: [], metrics: nil, views: views)
-            view.addConstraints(w + h)
-        }
-    }
-    @IBAction func butt(_ sender: Any) {
-        butt.isHidden = true
-        if let appDelegate = UIApplication.shared.delegate as? AppDelegate {
-            appDelegate.startUnity()
-            
-            NotificationCenter.default.addObserver(self, selector: #selector(handleUnityReady), name: NSNotification.Name("UnityReady"), object: nil)
-            NotificationCenter.default.addObserver(self, selector: #selector(handleUnityToggleRotation(_:)), name: NSNotification.Name("UnityToggleRotation"), object: nil)
-        }
-        handleUnityReady()
     }
 }
