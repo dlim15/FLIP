1b3d93f26e91bb23d9e42cfedf929cc2493e1f7c
Courtney Banh <c_banh@u.pacific.edu>
2018-03-19 16:27:07 -0700

    Created ScreenshotController.cs for testing

---
+++ ARKittyExample/Assets/Scripts/ScreenshotController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScreenshotController : MonoBehaviour {
+
+    public Button button;
+    
+    // Use this for initialization
+	void Start () {
+        Button btn = button.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void TaskOnClick()
+    {
+        GameObject[] ARObjects = GameObject.FindGameObjectsWithTag("ARObject");
+        foreach (GameObject ARObject in ARObjects)
+        {
+            ARObject.SetActive(false);
+        }
+
+        string filename = "Screenshot" + DateTime.Now.ToString("yyyyMMddHHmmss");
+        ScreenCapture.CaptureScreenshot(filename);
+
+        foreach (GameObject ARObject in ARObjects)
+        {
+            ARObject.SetActive(true);
+        }
+    }
+}
