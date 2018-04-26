baf6ec4be50d188340d79003cf58f6dab0aa22b6
Devin Lim <devin@opennetworking.org>
2018-04-26 14:48:30 -0700

    Created Tables for the database.

--- unitySwiftTest/unitySwiftTest/SqlCommand.swift
+++ unitySwiftTest/unitySwiftTest/SqlCommand.swift
@@ -21,7 +21,7 @@ class SqlCommand{
         
     }
     func createTable(){
-        let createTableQuery = "CREATE TABLE IF NOT EXISTS Picture (pid INTEGER PRIMARY KEY AUTOINCREMENT,fileName TEXT,surface INTEGER);CREATE TABLE IF NOT EXISTS Bundle (bid INTEGER,pid INTEGER,FOREIGN KEY (pid) REFERENCES Picture(pid),PRIMARY KEY(bid,pid)); "
+        let createTableQuery = "CREATE TABLE IF NOT EXISTS Picture(pId INTEGER,surface INTEGER,fileName TEXT,PRIMARY KEY(pId,surface));CREATE TABLE IF NOT EXISTS Object (objId INTEGER PRIMARY KEY AUTOINCREMENT,name TEXT,fileName TEXT);CREATE TABLE IF NOT EXISTS ObjectSpec(specId INTEGER,objId INTEGER,x_Coor REAL,y_Coor REAL,z_Coor REAL,x_rotate,y_rotate,z_rotate,x_scale,y_scale,z_scale, PRIMARY KEY(specId, objId)); CREATE TABLE IF NOT EXISTS ARSpace(spaceId INTEGER PRIMARY KEY AUTOINCREMENT,pId INTEGER,specId INTEGER,FOREIGN KEY (pId) REFERENCES Picture(pId),FOREIGN KEY (specId) REFERENCES ObjectSpec(specId)); "
         if sqlite3_exec(db, createTableQuery, nil, nil, nil) != SQLITE_OK{
             print("ERROR while creating table")
             return
