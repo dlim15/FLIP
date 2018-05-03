3513e1e595b087945556bc30dc64753ce7f33ec0
Jeremy Ronquillo <jeremyr@opennetworking.org>
2018-05-03 11:26:17 -0700

    Initial ability to move objects in ARRoom\!

--- unitySwiftTest/unitySwiftTest/ARRoomViewController.swift
+++ unitySwiftTest/unitySwiftTest/ARRoomViewController.swift
@@ -18,11 +18,25 @@ class ARRoomViewController: UIViewController, ARSCNViewDelegate {
     private var path : UIBezierPath!
     let sqlCommand = SqlCommand()
     var ARObjectStats : [String:[String:Any]]?
+    var roomItems : [String:SCNNode] = [String:SCNNode]()
+    var keyNum : Int = 0
+    var keys : [String] = [String]()
+    @IBOutlet weak var selectedItemTitle: UIButton!
+    @IBOutlet weak var toggleSelectedItem: UIButton!
     
+    @IBAction func ToggleSelectedItemButtonPressed(_ sender: Any) {
+        print("TOGGLE ITEM PRESSED")
+        keyNum = (keyNum + 1) % (ARObjectStats?.keys.count)!
+        selectedItemTitle.setTitle("Selected Item: \(keys[keyNum])", for: UIControlState.normal)
+    }
     
     //var planes = [ARPlaneAnchor: PlaneNode]()
     override func viewDidLoad() {
         super.viewDidLoad()
+        selectedItemTitle.isHidden = false
+        selectedItemTitle.setTitle("Look for a surface, then tap to create view.", for: UIControlState.normal)
+        toggleSelectedItem.isHidden = true
+        
         self.navigationController?.isNavigationBarHidden = false
         // Set the view's delegate
         sceneView.delegate = self
@@ -63,36 +77,19 @@ class ARRoomViewController: UIViewController, ARSCNViewDelegate {
             let touchLocation = touch.location(in: sceneView)
             // to perform to get the 3D coordinate corresponding to the 2D coord.
             // 3d coord will only be considered when it is on the existing plane we detected.
-            let results = sceneView.hitTest(touchLocation, types: .existingPlaneUsingExtent)
+            let results = sceneView.hitTest(touchLocation, types: .estimatedHorizontalPlane)
+            
             // check if we got some result using hitTest.
-            if touchCount == 0, let hitResult = results.first{
-                let boxScene = SCNScene(named: "art.scnassets/portal.scn")!
-                if let boxNode = boxScene.rootNode.childNode(withName: "portal", recursively: true){
-                    boxNode.position = SCNVector3(x:hitResult.worldTransform.columns.3.x,
-                                                  y:hitResult.worldTransform.columns.3.y + 0.05,
-                                                  z:hitResult.worldTransform.columns.3.z)
-                    boxNode.eulerAngles = SCNVector3(x:0.0,
-                                                     y:-90.0,
-                                                     z:0.0)
-
-                    if imgSet.count > 4{
-                        if let roomNode = boxScene.rootNode.childNode(withName: "room", recursively: true){
-                            for i in 0...imgSet.count - 1{
-                                let img : UIImage = UIImage(named: imgSet[i])!
-                                roomNode.geometry?.materials[i].diffuse.contents = img
-                            }
-                        }
-                        for key in (ARObjectStats?.keys)!{
-                            if let roomItem = boxScene.rootNode.childNode(withName: key, recursively: true){
-                                roomItem.position = setObjectPositionInSwift(objectKey: key)
-//                            tableNode.eulerAngles = setObjectEulerInSwift(objectKey: key)
-                                roomItem.scale = setObjectScaleInSwift(objectKey: key)
-//                                roomItem.isHidden = false
-                            }
-                        }
-                    }
-                    sceneView.scene.rootNode.addChildNode(boxNode)
-                    touchCount += 1
+            if let hitResult = results.first{
+                if (touchCount == 0){
+                    initRoom(hitResult: hitResult)
+                } else {
+                    print("******** HIT RESULT: \(hitResult)")
+                    let key = keys[keyNum]
+                    roomItems[key]?.worldPosition = SCNVector3(x:hitResult.worldTransform.columns.3.x,
+                                                               y:-0.263,
+                                                               z:hitResult.worldTransform.columns.3.z)
+                    
                 }
             }
         }
@@ -103,7 +100,44 @@ class ARRoomViewController: UIViewController, ARSCNViewDelegate {
 //        print(nodeId)
     }
     
-    func setObjectPositionInSwift( objectKey : String ) -> SCNVector3 {
+    func initRoom(hitResult : ARHitTestResult){
+        let boxScene = SCNScene(named: "art.scnassets/portal.scn")!
+        if let boxNode = boxScene.rootNode.childNode(withName: "portal", recursively: true){
+            boxNode.position = SCNVector3(x:hitResult.worldTransform.columns.3.x,
+                                          y:hitResult.worldTransform.columns.3.y + 0.05,
+                                          z:hitResult.worldTransform.columns.3.z)
+            boxNode.eulerAngles = SCNVector3(x:0.0,
+                                             y:-90.0,
+                                             z:0.0)
+            
+            if imgSet.count > 4{
+                if let roomNode = boxScene.rootNode.childNode(withName: "room", recursively: true){
+                    for i in 0...imgSet.count - 1{
+                        let img : UIImage = UIImage(named: imgSet[i])!
+                        roomNode.geometry?.materials[i].diffuse.contents = img
+                    }
+                }
+                for key in (ARObjectStats?.keys)!{
+                    if let roomItem = boxScene.rootNode.childNode(withName: key, recursively: true){
+                        roomItems[roomItem.name!] = roomItem
+                        roomItem.position = setObjectPositionFromDictInSwift(objectKey: key)
+                        //                            tableNode.eulerAngles = setObjectEulerInSwift(objectKey: key)
+                        roomItem.scale = setObjectScaleFromDictInSwift(objectKey: key)
+                        //                                roomItem.isHidden = false
+                        keys.append(key)
+                    }
+                }
+            }
+            sceneView.scene.rootNode.addChildNode(boxNode)
+            keyNum = 0
+            selectedItemTitle.isHidden = false
+            toggleSelectedItem.isHidden = false
+            selectedItemTitle.setTitle("Selected Item: \(keys[keyNum])", for: UIControlState.normal)
+            touchCount += 1
+        }
+    }
+    
+    func setObjectPositionFromDictInSwift( objectKey : String ) -> SCNVector3 {
         let floorYValues : [String:Float] = ["table" : -0.213,
                                              "toliet" : -0.063,
                                              "plant1" : -0.263,
@@ -124,7 +158,7 @@ class ARRoomViewController: UIViewController, ARSCNViewDelegate {
         return result
     }
     
-    func setObjectScaleInSwift( objectKey : String ) -> SCNVector3 {
+    func setObjectScaleFromDictInSwift( objectKey : String ) -> SCNVector3 {
         let baseScaleValues : [String:Float] = ["table" : 0.016666667,
                                                 "toliet" : 0.06,
                                                 "plant1" : 0.04,
